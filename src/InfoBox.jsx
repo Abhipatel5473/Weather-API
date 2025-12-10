db35759132d2137ac66631ba162f48cd94213156
Abhi Patel <abhipatel5473@gmail.com>
2025-12-10 20:46:43 +0530

    add icons

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -2,6 +2,9 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import WbSunnyRoundedIcon from '@mui/icons-material/WbSunnyRounded';
+import ThunderstormRoundedIcon from '@mui/icons-material/ThunderstormRounded';
+import AcUnitRoundedIcon from '@mui/icons-material/AcUnitRounded';
 
 function InfoBox({ info }) {
     const INIT_URL = "https://images.unsplash.com/photo-1764957080878-3f9866270aad?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8";
@@ -25,7 +28,12 @@ function InfoBox({ info }) {
                 />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: "bold", fontSize: "2rem" }}>
-                        {info.City}
+                        {info.City} {info.Humidity > 90
+                        ? <ThunderstormRoundedIcon />
+                        : info.Temp > 20 
+                        ? <WbSunnyRoundedIcon />  
+                        : <AcUnitRoundedIcon />
+                    }
                     </Typography>
                     <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
                         <p>Temperature: {info.Temp}&deg;C</p>
